bc499f11b3bb9cd6f7dd3212a1949688dba2da9c
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-11-08 22:20:47 -0300

    feat(back-end): update adocao.route from PUT to PATCH

--- back-end/src/routes/adocao.route.js
+++ back-end/src/routes/adocao.route.js
@@ -7,7 +7,7 @@ const adocaoController = new AdocaoController();
 router.post("/", adocaoController.criarNovaAdocao);
 router.get("/", adocaoController.buscarAdocoes);
 router.get("/:id", adocaoController.buscarPorId);
-router.put("/:id", adocaoController.atualizarAdocao);
+router.patch("/:id", adocaoController.atualizarAdocao);
 router.delete("/:id", adocaoController.deletarAdocao);
 
 export default router;
