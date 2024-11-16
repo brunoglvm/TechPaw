4a01369f2fa8aa7709fc9dfbe8d47886fcb6350c
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-11-16 14:59:41 -0300

    update*

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,14 @@ import helmet from "helmet";
 import routes from "./routes/index.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em: http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em: http://localhost:${PORT}`);
   // Rotas disponíveis: /api/adocoes, /api/adotantes, /api/pets
 });
